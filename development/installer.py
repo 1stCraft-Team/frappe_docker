dfde9fc1b81000734067a0aa051ff30bdab52626
Waratape-Oam <oam@1stcraft.com>
2025-07-21 18:22:11 +0700

    make installer.py has backward compatibility

--- development/installer.py
+++ development/installer.py
@@ -210,7 +210,7 @@ def create_site_in_bench(args):
             f"--db-root-username=root",
             f"--db-host=mariadb",  # Should match the compose service name
             f"--db-type={args.db_type}",  # Add the selected database type
-            f"--mariadb-user-host-login-scope=%",
+            f"--no-mariadb-socket",
             f"--db-root-password=123",  # Replace with your MariaDB password
             f"--admin-password={args.admin_password}",
         ]
